ee5d59e1dfb72b01b3f93edfe7407ec1c1a1a8a8
sabujAlom <sabujalom18@gmail.com>
2026-09-03 10:07:24 +0600

    inerface ts practice

--- ineteface.ts
+++ ineteface.ts
@@ -9,4 +9,54 @@ const user: User = {
   age: 26,
   gmail: "sabujalom18@gmail.com"
 };
-console.log(user)
+
+
+
+
+
+
+
+interface Student {
+  name: string;
+  age: number;
+  email: string;
+
+  address: {
+     city:string;
+     country:string;
+  };
+}
+
+
+
+
+// একটা Employee interface তৈরি করো।
+// Requirements:
+// name → string required
+// salary → number required
+// department → string required
+// phone → string optional
+// address → optional object
+// city → string
+// country → string
+
+interface Employee{
+    name:string;
+    salary: number;
+    department: string;
+    phone?: string;
+    address?:{
+        city:string;
+        country: string;
+    }
+}
+const employee: Employee = {
+       name: "sabuj",
+       salary: 70000,
+       department:"tech",
+       address:{
+        city:"Dhaka",
+        country:"Bangladesh"
+       }
+};
+console.log(employee)
