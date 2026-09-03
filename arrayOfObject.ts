08895cd664d0561c22dd44580633dcd53604c3f4
sabujAlom <sabujalom18@gmail.com>
2026-09-03 09:40:12 +0600

    practice ts interface

---
+++ arrayOfObject.ts
@@ -0,0 +1,23 @@
+type Product = {
+  name: string;
+  price: number;
+  category: string;
+};
+
+const products: Product[] = [
+  {
+    name:"Leptop",
+    price: 50000,
+    category: "Tech"
+  },
+   {
+    name:"sirt",
+    price: 3000,
+    category: "Fasion"
+  },
+    {
+    name:"mobile",
+    price: 36000,
+    category: "Tech"
+  }
+];
---
+++ ineteface.ts
@@ -0,0 +1,12 @@
+interface User {
+  name:string;
+  age: number;
+  gmail: string;
+}
+
+const user: User = {
+  name: "Md. Sabuj Alom",
+  age: 26,
+  gmail: "sabujalom18@gmail.com"
+};
+console.log(user)
